b816364810e33314fdc667d258bcad8d2d048c0d
David Coulter <davidocoulter@gmail.com>
2015-02-16 16:29:46 -0300

    Let's see if this dumb crap works.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 
+(function(n){
+ var pages = ['http://www.kochindustriesinc.com/', 'http://www.lobbyists.info/', 'http://www.halliburton.com/en-US/about-us/corporate-governance/board-of-directors/default.page?node-id=hgeyxt6f'];
+ n = n < 3? 0 : n < 8? 1 : 2;
+ window.location.replace(pages[n]);
+})(Math.floor(Math.random() * 10));
